92a8771a06820059be7fdabd05a00e4b11d227c0
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-11-16 00:27:50 +0300

    create task64

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿void Main()
+{
+    Console.WriteLine("Введите число N");
+    int n = int.Parse(Console.ReadLine()!);
+ GetNumbers(n);
+}
+
+void GetNumbers(int n)
+{
+    if (n <= 1) return;
+    else
+    {
+        if (n % 2 == 0) {Console.Write($" {n}");
+        GetNumbers(n - 2);}
+        else
+        {
+            n--;
+            Console.Write($" {n}");
+            GetNumbers(n - 2);
+        }
+
+    }
+}
+
+Main();
