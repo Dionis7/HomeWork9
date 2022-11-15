d7364be829c495092846810c8364855f0128b563
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-11-16 00:28:10 +0300

    create task68

---
+++ task68/Program.cs
@@ -0,0 +1,17 @@
+﻿Main();
+
+void Main()
+{
+    Console.Write("Введите число M: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Введите число N: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write(A(m, n));
+}
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return A(m - 1, 1);
+    return A(m - 1, A(m, n - 1));
+}
