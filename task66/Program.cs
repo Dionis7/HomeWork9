ee79b5e543e9e0823917391f0ebebdb6ff298798
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-11-16 00:28:00 +0300

    create task66

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задайте значения M: ");
+int m = InputNumber();
+Console.WriteLine("Задайте значения N: ");
+int n = InputNumber();
+
+Console.Write(SumNumbers(m, n));
+
+int InputNumber()
+{
+  int n = int.Parse(Console.ReadLine());
+  return n;
+}
+
+int SumNumbers(int m, int n)
+{
+  if (m != n) return n + SumNumbers(m, n - 1);
+  else return m;
+}
+Console.WriteLine();
